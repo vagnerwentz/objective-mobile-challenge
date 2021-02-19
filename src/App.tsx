373e1b3fe004d5dc404f148524e08cac275456da
Vagner Wentz <wentz.vagner@gmail.com>
2021-02-19 20:14:42 -0300

    feat: changed the initial structure to run the application.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return (
+    <>
+      <View>
+        <Text>Oi</Text>
+      </View>
+    </>
+  );
+};
+export default App;
