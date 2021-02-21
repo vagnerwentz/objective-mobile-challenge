5b43123087339558adcad590c5e1945d03fb8d93
Vagner Wentz <wentz.vagner@gmail.com>
2021-02-20 21:32:45 -0300

    feat: created the hero list screen.

--- src/components/Heros/List/index.tsx
+++ src/components/Heros/List/index.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { api } from '../../../services/marvelApi';
+
+import { HeroIndexProtocol  } from './interfaces/hero-protocols';
 
 import {
   HerosListHeader,
@@ -7,13 +11,31 @@ import {
 
 import { Content } from './Content';
 
+import { MARVEL_API } from '@env';
+
 const HeroList = () => {
+  const [heroes, setHeroes] = useState<HeroIndexProtocol[]>([]);
+
+  useEffect(() => {
+    api.get(`${MARVEL_API}/v1/public/characters?limit=4`).then((
+      response => setHeroes(response.data.data.results)
+    ));
+  }, []);
+
   return (
     <>
       <HerosListHeader>
         <HeaderContent>Nome</HeaderContent>
       </HerosListHeader>
-      <Content />
+      {heroes.map((hero => (
+        <Content
+          key={hero.id}
+          id={hero.id}
+          name={hero.name}
+          thumbnailURL={`${hero.thumbnail.path}/standard_medium.${hero.thumbnail.extension}`}
+        />
+      )))}
+
     </>
   );
 }
