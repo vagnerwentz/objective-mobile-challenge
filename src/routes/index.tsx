ff9a3fbd2529908673f75e14658cbffa96090408
Vagner Wentz <wentz.vagner@gmail.com>
2021-02-21 10:28:19 -0300

    feat: added the Detail screen to routes.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import { Home } from '../pages/Home';
+import { Detail } from '../components/Heros/Detail';
 
 const App = createStackNavigator();
 
@@ -14,6 +15,7 @@ const AppRoutes = () => {
     }}
   >
       <App.Screen name="Home" component={Home} />
+      <App.Screen name="Detail" component={Detail} />
     </App.Navigator>
   )
 }
