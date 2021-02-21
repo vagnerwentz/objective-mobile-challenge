245d5a2d54bbd46b5c52f2209cef75c8c5acb952
Vagner Wentz <wentz.vagner@gmail.com>
2021-02-20 21:30:54 -0300

    chore: configured the .env files.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: ['MARVEL_API', 'MARVEL_PUBLIC_KEY', 'MARVEL_PRIVATE_KEY'],
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
+  ],
 };
---
+++ src/types/env.d.ts
@@ -0,0 +1,5 @@
+declare module '@env' {
+  export const MARVEL_API: string;
+  export const MARVEL_PUBLIC_KEY: string;
+  export const MARVEL_PRIVATE_KEY: string;
+}
