869562d0f29990075455c4b1499090cc62bf8b2a
Vagner Wentz <wentz.vagner@gmail.com>
2021-02-21 22:28:22 -0300

    feat: created the HeroAppearances component and added the context function created to handle the type value about the badge.

---
+++ src/components/Heros/Detail/components/HeroApperances/index.tsx
@@ -0,0 +1,67 @@
+import React, { useContext } from 'react';
+import { HeroContext } from '../../../../../hooks/context/heroContext';
+
+import { HeroDetailAppearanceProps } from '../interfaces/hero-detail-protocols';
+
+import {
+  BadgeContent,
+  Badge,
+  BadgeDescription,
+  BadgeViewValue,
+  BadgeValue,
+ } from './styles';
+
+enum ButtonTypeValue {
+  STORIES = 'stories',
+  SERIES = 'series',
+  EVENTS = 'events',
+  COMICS = 'comics',
+}
+
+const HeroApperances = ({stories, comics, events, series}: HeroDetailAppearanceProps) => {
+  const { handleViewTheHeroApparitions } = useContext(HeroContext);
+
+  return (
+    <>
+      <BadgeContent horizontal showsHorizontalScrollIndicator={false}>
+        {stories !== 0 && (
+           <Badge type="stories" onPress={() => handleViewTheHeroApparitions(ButtonTypeValue.STORIES)}>
+              <BadgeDescription >Estórias</BadgeDescription>
+              <BadgeViewValue type="stories">
+                  <BadgeValue>{stories}</BadgeValue>
+              </BadgeViewValue>
+           </Badge>
+        )}
+
+        {series !== 0 && (
+          <Badge type="series" onPress={() => handleViewTheHeroApparitions(ButtonTypeValue.SERIES)}>
+            <BadgeDescription >Séries</BadgeDescription>
+            <BadgeViewValue type="series">
+                <BadgeValue>{series}</BadgeValue>
+              </BadgeViewValue>
+          </Badge>
+        )}
+
+        {events !== 0 && (
+          <Badge type="events" onPress={() => handleViewTheHeroApparitions(ButtonTypeValue.EVENTS)}>
+            <BadgeDescription >Eventos</BadgeDescription>
+            <BadgeViewValue type="events">
+                <BadgeValue>{events}</BadgeValue>
+              </BadgeViewValue>
+          </Badge>
+        )}
+
+        {comics !== 0 && (
+          <Badge type="comics" onPress={() => handleViewTheHeroApparitions(ButtonTypeValue.COMICS)}>
+            <BadgeDescription >HQs</BadgeDescription>
+            <BadgeViewValue type="comics">
+                <BadgeValue>{comics}</BadgeValue>
+              </BadgeViewValue>
+          </Badge>
+        )}
+      </BadgeContent>
+    </>
+  );
+}
+
+export { HeroApperances };
---
+++ src/components/Heros/Detail/components/HeroApperances/styles.ts
@@ -0,0 +1,67 @@
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import styled, {css} from 'styled-components/native';
+
+interface BadgeColor {
+  type: string;
+}
+
+export const BadgeContent = styled.ScrollView`
+  display: flex;
+  flex-direction: row;
+  margin-top: 5px;
+  width: 100%;
+  flex-wrap: wrap;
+  padding: 0px 15px 15px 0px;
+`;
+
+export const Badge = styled(TouchableOpacity)<BadgeColor>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px 10px 10px 5px;
+  border-radius: 30px;
+  justify-content: center;
+  margin-right: 5px;
+  margin-left: 5px;
+
+  background-color: ${
+    props => (props.type === 'stories' ? '#FB5555' : (
+      props.type === 'series' ? '#F4A623' : (
+        props.type === 'events' ? '#FC0080' : (
+          '#0070F3'
+        )
+      )
+    ))
+  };
+`;
+
+export const BadgeDescription = styled.Text`
+  font-size: 18px;
+  color: #fff;
+  font-family: 'Roboto-Regular';
+`;
+
+export const BadgeViewValue = styled.View<BadgeColor>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 15px;
+  width: 30px;
+  height: 30px;
+  margin-left: 5px;
+  background-color: ${
+    props => (props.type === 'stories' ? '#C94444' : (
+      props.type === 'series' ? '#C3851C' : (
+        props.type === 'events' ? '#CA0067' : (
+          '#005AC3'
+        )
+      )
+    ))
+  };
+`;
+
+export const BadgeValue = styled.Text`
+  font-size: 18px;
+  color: #fff;
+  font-family: 'Roboto-Regular';
+`;
