a2b1207c89247e582ef41837324ec116c405f75e
Vagner Wentz <wentz.vagner@gmail.com>
2021-02-21 22:25:20 -0300

    feat: created the interfaces.

---
+++ src/components/Heros/Detail/components/interfaces/hero-detail-protocols.ts
@@ -0,0 +1,13 @@
+import { ContentApperedProps } from '../../../List/interfaces/hero-protocols';
+
+export interface HeroDetailAppearanceProps {
+  comics: number;
+  series: number;
+  stories: number;
+  events: number;
+};
+
+export interface HeroItemApperedOn {
+  id: number;
+};
+
--- src/components/Heros/List/interfaces/hero-protocols.ts
+++ src/components/Heros/List/interfaces/hero-protocols.ts
@@ -25,17 +25,112 @@ export interface HeroListProtocol {
   }
 };
 
+export interface MarvelHeroShowCharacter {
+  data: {
+    results: {
+      id: number
+      name: string
+      description: string
+      thumbnail: {
+        path: string
+        extension: string
+      }
+      comics: {
+        available: number
+        items: {
+          resourceURI: string
+          name: string
+        }[]
+      }
+      events: {
+        available: number
+        items: {
+          resourceURI: string
+          name: string
+        }[]
+      }
+      series: {
+        available: number
+        items: {
+          resourceURI: string
+          name: string
+        }[]
+      }
+      stories: {
+        available: number
+        items: {
+          resourceURI: string
+          name: string
+        }[]
+      }
+    }[]
+  }
+}
+
 export interface HeroIndexProtocol {
   id: number;
   name: string;
+  description: string;
   thumbnail: {
     path: string;
     extension: string;
+  };
+  comics: {
+    available: number;
+    items: {
+      resourceURI: string;
+      name: string;
+    }[]
+  }
+  events: {
+    available: number;
+    items: {
+      resourceURI: string;
+      name: string;
+    }[]
+  }
+  series: {
+    available: number;
+    items: {
+      resourceURI: string;
+      name: string;
+    }[]
+  }
+  stories: {
+    available: number;
+    items: {
+      resourceURI: string;
+      name: string;
+    }[]
+  }
+};
+
+export interface ContentApperedProps {
+  available: number;
+  items: {
+    resourceURI: string;
+    name: string;
+  }[]
+}
+
+export interface HeroDetail {
+  route: {
+    params: {
+      id: number;
+      name: string;
+      description: string;
+      thumbnail: {
+        path: string;
+        extension: string;
+      };
+      comics: ContentApperedProps;
+      events: ContentApperedProps;
+      series: ContentApperedProps;
+      stories: ContentApperedProps;
+    }
   }
 };
 
 export interface HeroContentProps {
-  id: number;
-  thumbnailURL: string;
-  name: string;
+  data: HeroIndexProtocol;
 }
