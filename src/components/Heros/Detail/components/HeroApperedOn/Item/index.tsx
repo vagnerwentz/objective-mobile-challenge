930b27f6d7898b927f1b02ed18a8e5b65f5103b9
Vagner Wentz <wentz.vagner@gmail.com>
2021-02-21 22:31:49 -0300

    feat: created the structure to show the image and name about the appearance.

---
+++ src/components/Heros/Detail/components/HeroApperedOn/Item/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {
+  ContentContainer,
+  ImageApperedOn,
+  ContainerText,
+  Type,
+} from './styles';
+
+interface ItemProps {
+  name: string;
+  imageUri: string;
+}
+
+const ItemFlatList = ({ name, imageUri }: ItemProps) => {
+  return (
+    <ContentContainer>
+      <ImageApperedOn source={{uri: imageUri}} />
+      <ContainerText>
+        <Type>{name}</Type>
+      </ContainerText>
+    </ContentContainer>
+  );
+}
+
+export { ItemFlatList };
---
+++ src/components/Heros/Detail/components/HeroApperedOn/Item/styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components/native';
+
+export const ContentContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
+
+export const ImageApperedOn = styled.Image`
+  height: 80px;
+  width: 80px;
+  border-radius: 10px;
+  background-color: #000;
+`;
+
+export const ContainerText = styled.View`
+  display: flex;
+  justify-content: center;
+  width: 200px;
+`;
+
+export const Type = styled.Text`
+  font-size: 18px;
+  padding-left: 10px;
+`;
