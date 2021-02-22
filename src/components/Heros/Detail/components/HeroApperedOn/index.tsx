175d03c05d55df4150ac8f452a0e130b04ae1c69
Vagner Wentz <wentz.vagner@gmail.com>
2021-02-21 22:30:51 -0300

    feat: created the HeroAppearedOn component to list the appeared events, comics, series and stories.

---
+++ src/components/Heros/Detail/components/HeroApperedOn/index.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useContext, useState } from 'react';
+import { FlatList } from 'react-native';
+import { HeroContext } from '../../../../../hooks/context/heroContext';
+
+import { HeroItemApperedOn } from '../interfaces/hero-detail-protocols';
+
+import { api } from '../../../../../services/marvelApi';
+
+import { ItemFlatList } from './Item';
+
+import {
+  ApperedOnContainer,
+  Title,
+} from './styles';
+
+import { MARVEL_API } from '@env';
+
+interface AppearedListProps {
+  results: Array<{
+    id: string;
+    title: string;
+    thumbnail: {
+      extension: string;
+      path: string;
+    }
+  }>
+}
+
+const HeroApperedOn = ({ id }: HeroItemApperedOn) => {
+  const { appearedType } = useContext(HeroContext);
+  const [appearedData, setAppearedData] = useState<AppearedListProps>({} as AppearedListProps);
+
+  useEffect(() => {
+    api.get(`${MARVEL_API}/v1/public/characters/${id}/${appearedType}`).then((response) =>
+    setAppearedData(response.data.data),
+  )}, [appearedType]);
+
+  return (
+    <>
+      <ApperedOnContainer>
+        <Title>Vamos ver onde este herói apareceu</Title>
+        <FlatList
+          extraData
+          data={appearedData.results}
+          renderItem={(data) => <ItemFlatList
+            name={data.item.title}
+            imageUri={
+              data.item.thumbnail ?
+              `${data.item.thumbnail.path}/portrait_uncanny.${data.item.thumbnail.extension}`
+              : 'http://www.freeiconspng.com/img/23541'
+            }
+          />}
+          keyExtractor={item => `${item.id}`}
+        />
+
+      </ApperedOnContainer>
+    </>
+  );
+}
+
+export { HeroApperedOn };
---
+++ src/components/Heros/Detail/components/HeroApperedOn/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+const grayColor = '#4E4E4E';
+
+export const ApperedOnContainer = styled.View`
+  display: flex;
+  flex: 1;
+  margin-left: 20px;
+`;
+
+export const Title = styled.Text`
+  font-size: 18px;
+  font-family: 'Roboto-Regular';
+  color: ${grayColor};
+`;
