cbc02c57dfaea1b77e8bd233a0d2d9d94861cbab
Vagner Wentz <wentz.vagner@gmail.com>
2021-02-21 22:26:48 -0300

    feat: added the context function created to handle the list heroes.

--- src/components/Heros/List/index.tsx
+++ src/components/Heros/List/index.tsx
@@ -1,8 +1,5 @@
-import React, { useEffect, useState } from 'react';
-
-import { api } from '../../../services/marvelApi';
-
-import { HeroIndexProtocol  } from './interfaces/hero-protocols';
+import React, { useContext, useEffect } from 'react';
+import { HeroContext } from '../../../hooks/context/heroContext';
 
 import {
   HerosListHeader,
@@ -11,15 +8,11 @@ import {
 
 import { Content } from './Content';
 
-import { MARVEL_API } from '@env';
-
 const HeroList = () => {
-  const [heroes, setHeroes] = useState<HeroIndexProtocol[]>([]);
+  const { heroes, handleCallAPIToListHeroes } = useContext(HeroContext);
 
   useEffect(() => {
-    api.get(`${MARVEL_API}/v1/public/characters?limit=5`).then((
-      response => setHeroes(response.data.data.results)
-    ));
+    handleCallAPIToListHeroes();
   }, []);
 
   return (
