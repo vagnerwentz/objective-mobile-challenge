63f8d6d96ccfe575b9f321d06e8679db266f6090
Vagner Wentz <wentz.vagner@gmail.com>
2021-02-22 23:50:19 -0300

    feat: created contexts functions to handle the hero and search hero by name.

--- src/hooks/context/heroContext.tsx
+++ src/hooks/context/heroContext.tsx
@@ -1,35 +1,62 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useCallback, useEffect, useState } from 'react';
 
 import { MARVEL_API } from '@env';
 
 import { api } from '../../services/marvelApi';
 
 import { HeroIndexProtocol } from '../../components/Heros/List/interfaces/hero-protocols';
+interface HeroName {
+  [key: string]: string;
+}
 
 interface HeroContextProps {
   appearedType: string;
   hero: HeroIndexProtocol;
   heroes: HeroIndexProtocol[];
-  handleCallAPIToListHeroes(): Promise<void>;
+  totalOfHeroes: number;
+  filteredHero: HeroIndexProtocol[];
+  loading: boolean;
+  handleCallAPIToListHeroes(offset: number): Promise<void>;
   handleCallAPIToListTheHeroDetail(heroId: number): Promise<void>;
   handleViewTheHeroApparitions(type: string): void;
+  searchHero(searchName: HeroName): void;
 }
 
 export const HeroContext = createContext<HeroContextProps>(
   {} as HeroContextProps
 );
 
+interface SearchHeroProps {
+  data: {
+    results: Array<{
+      name: string;
+      thumbnail: {
+        extension: string;
+        path: string;
+      }
+    }>
+  }
+}
+
 export const HeroProvider: React.FC = ({ children }) => {
   const [appearedType, setAppearedType] = useState<string>('');
+  const [filteredHero, setFilteredHero] = useState<HeroIndexProtocol[]>([]);
   const [heroes, setHeroes] = useState<HeroIndexProtocol[]>([]);
+  const [totalOfHeroes, setTotalOfHeroes] = useState<number>(0);
+  const [loading, setLoading] = useState(true);
   const [hero, setHero] = useState<HeroIndexProtocol>({} as HeroIndexProtocol);
 
-  const handleViewTheHeroApparitions = (type: string) => {
+  const handleViewTheHeroApparitions = (type: string = 'stories') => {
     setAppearedType(type);
   }
 
-  async function handleCallAPIToListHeroes() {
-    const { data } = await api.get(`${MARVEL_API}/v1/public/characters?limit=5`);
+  async function handleCallAPIToListHeroes(offset: number = 0) {
+    setLoading(true);
+    const { data } = await api.get
+      (`${MARVEL_API}/v1/public/characters?limit=5&offset=${offset}`)
+    ;
+    setLoading(false);
+    setTotalOfHeroes(data.data.total);
     setHeroes(data.data.results);
   }
 
@@ -38,12 +65,23 @@ export const HeroProvider: React.FC = ({ children }) => {
     setHero(data);
   }
 
+  const searchHero = useCallback(async ({ heroName }: HeroName) => {
+    const results = heroes.filter(({ name }) =>
+      name.includes(heroName),
+    );
+    setFilteredHero(results);
+  }, []);
+
   return (
     <HeroContext.Provider
       value={{
         hero,
         heroes,
+        loading,
+        searchHero,
+        filteredHero,
         appearedType,
+        totalOfHeroes,
         handleCallAPIToListHeroes,
         handleViewTheHeroApparitions,
         handleCallAPIToListTheHeroDetail,
