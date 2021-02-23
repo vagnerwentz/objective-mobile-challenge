385da7c04a4c90c3c4cb6027ead5b23fd863975a
Vagner Wentz <wentz.vagner@gmail.com>
2021-02-23 12:28:14 -0300

    chore: eslint auto adjust.

--- src/components/Shimmer/ShimmerContentListHero/index.tsx
+++ src/components/Shimmer/ShimmerContentListHero/index.tsx
@@ -1,22 +1,25 @@
-import React from "react";
-import { View } from "react-native";
-import SkeletonPlaceholder from "react-native-skeleton-placeholder";
+import React from 'react';
+import { View } from 'react-native';
+import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
 
 const ShimmerContentListHero = () => {
   return (
     <SkeletonPlaceholder>
-      <View style={{
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        marginTop: 18,
-        marginLeft: 80
-      }}>
-        <View style={{
-          width: 56,
-          height: 56,
-          borderRadius: 28
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          marginTop: 18,
+          marginLeft: 80,
         }}
+      >
+        <View
+          style={{
+            width: 56,
+            height: 56,
+            borderRadius: 28,
+          }}
         />
 
         <View style={{ marginLeft: 20 }}>
