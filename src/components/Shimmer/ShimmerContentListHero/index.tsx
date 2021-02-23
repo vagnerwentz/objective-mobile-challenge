adc9d0c9dedce410e3e07b9eb9cafe360c8d9afe
Vagner Wentz <wentz.vagner@gmail.com>
2021-02-22 23:51:31 -0300

    feat: created the shimmer component.

---
+++ src/components/Shimmer/ShimmerContentListHero/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { View } from "react-native";
+import SkeletonPlaceholder from "react-native-skeleton-placeholder";
+
+const ShimmerContentListHero = () => {
+  return (
+    <SkeletonPlaceholder>
+      <View style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        marginTop: 18,
+        marginLeft: 80
+      }}>
+        <View style={{
+          width: 56,
+          height: 56,
+          borderRadius: 28
+        }}
+        />
+
+        <View style={{ marginLeft: 20 }}>
+          <View style={{ width: 120, height: 20, borderRadius: 4 }} />
+        </View>
+      </View>
+    </SkeletonPlaceholder>
+  );
+};
+
+export { ShimmerContentListHero };
